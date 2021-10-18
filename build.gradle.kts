7eb7850c3cd52497d45b2ec0ecf9dd1a135ff2f4
dongyoun_shin <dyshin1027@gmail.com>
2021-10-18 22:09:07 +0900

    [dev] 의존성 정리

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,44 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+buildscript {
+	repositories {
+		jcenter()
+	}
+	dependencies {
+		classpath("gradle.plugin.com.ewerk.gradle.plugins:querydsl-plugin:1.0.10")
+	}
+}
+
 plugins {
 	id("org.springframework.boot") version "2.5.5"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.5.31"
 	kotlin("plugin.spring") version "1.5.31"
+	id("com.gorylenko.gradle-git-properties") version "1.5.1"
 }
+
 
 group = "com.exercise"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
+configurations.forEach {
+	it.exclude("org.springframework.boot", "spring-boot-starter-tomcat")
+}
 repositories {
-	mavenCentral()
+	jcenter()
 }
 
 extra["springCloudVersion"] = "2020.0.4"
 
 dependencies {
+	implementation("com.querydsl:querydsl-jpa:4.2.1")
+	implementation("com.querydsl:querydsl-apt:4.2.1")
+	implementation("com.querydsl:querydsl-sql-spring:4.2.1")
+	kapt("com.querydsl:querydsl-apt:4.2.1:jpa")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-amqp")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
@@ -30,6 +50,12 @@ dependencies {
 	runtimeOnly("mysql:mysql-connector-java")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.amqp:spring-rabbit-test")
+	kapt("org.springframework.boot:spring-boot-configuration-processor")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	runtimeOnly("com.h2database:h2")
+	testImplementation("org.junit.jupiter", "junit-jupiter-api")
+	testImplementation("org.mockito:mockito-core:3.1.0")
+	testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine")
 }
 
 dependencyManagement {
@@ -39,6 +65,18 @@ dependencyManagement {
 }
 
 tasks.withType<KotlinCompile> {
+	doFirst {
+		val git = org.ajoberstar.grgit.Grgit.open {
+			file(".")
+		}
+		val infoFile = file("${projectDir}/src/main/resources/build-info.properties")
+		val properties = Properties()
+		val localDateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+		properties.setProperty("info.version", "1.1.4")
+		properties.setProperty("info.git.hash", git.head().getAbbreviatedId(7))
+		properties.setProperty("info.git.buildDate", localDateTime)
+		properties.store(infoFile.writer(), "Build Information")
+	}
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
 		jvmTarget = "1.8"
@@ -47,4 +85,21 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+val querydslSourcesDir = file("src/main/generated")
+
+sourceSets {
+	main {
+		java {
+			listOf("src/main/java", querydslSourcesDir)
+		}
+		kotlin {
+			listOf("src/main/kotlin", querydslSourcesDir)
+		}
+	}
+}
+
+tasks.getByName<BootJar>("bootJar") {
+	enabled = true
 }
