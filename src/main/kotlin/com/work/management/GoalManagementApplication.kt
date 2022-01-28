61cb456431911e9cbade3dfca42b76370227ce7e
dongyoun_shin <dyshin1027@gmail.com>
2022-01-29 03:27:40 +0900

    [dev] 목표달성 도움 프로젝트 시작

--- src/main/kotlin/com/work/management/GoalManagementApplication.kt
+++ src/main/kotlin/com/work/management/GoalManagementApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class WorkManagementApplication
+class GoalManagementApplication
 
 fun main(args: Array<String>) {
-	runApplication<WorkManagementApplication>(*args)
+	runApplication<GoalManagementApplication>(*args)
 }
