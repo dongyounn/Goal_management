4c1448d60ce31fecd1b270013f690fbc2aa6f4b6
dongyoun_shin <dyshin1027@gmail.com>
2022-02-01 01:23:05 +0900

    [dev] flyway 수정, 유저 성별 타입 추가

--- src/main/resources/db/migration/V1.2__CreateUserTable.sql
+++ src/main/resources/db/migration/V1.2__CreateUserTable.sql
@@ -6,6 +6,7 @@ CREATE TABLE user
     phone_number        varchar(50)  not null comment '전화번호',
     user_address        varchar(255) not null comment '유저 주소',
     user_detail_address varchar(255) not null comment '상세 주소',
+    gender              varchar(6)   not null comment '성별',
     created             datetime,
     updated             datetime
 );
