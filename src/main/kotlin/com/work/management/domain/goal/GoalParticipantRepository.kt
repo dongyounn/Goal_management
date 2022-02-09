5ae894cdbdcf0d9977eec0ae86af9ab1574fdfb7
dongyoun_shin <dyshin1027@gmail.com>
2022-02-10 00:14:08 +0900

    [dev] 목표 도전자 관리 도메인 개발중

--- src/main/kotlin/com/work/management/domain/goal/GoalParticipantRepository.kt
+++ src/main/kotlin/com/work/management/domain/goal/GoalParticipantRepository.kt
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface GoalRepository : JpaRepository<Goal, Long>
-
+interface GoalParticipantRepository : JpaRepository<GoalParticipants, Long>
