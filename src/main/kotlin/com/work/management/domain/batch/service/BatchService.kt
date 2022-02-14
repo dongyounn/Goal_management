ed2e1660f61ec50b77712020bc8b97d7e533f598
dongyoun_shin <dyshin1027@gmail.com>
2022-02-15 00:14:30 +0900

    [dev] 배치 구조 세팅

---
+++ src/main/kotlin/com/work/management/domain/batch/service/BatchService.kt
@@ -0,0 +1,21 @@
+package com.work.management.domain.batch.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class BatchService {
+
+    fun read() {
+
+    }
+
+    fun process() {
+
+    }
+
+    fun write() {
+
+    }
+
+
+}
